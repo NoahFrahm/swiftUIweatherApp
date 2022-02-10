90345b122947c3ae3907f3eabc036a2680281e03
NoahFrahm <69765035+NoahFrahm@users.noreply.github.com>
2022-02-10 17:03:26 -0500

    Add files via upload

---
+++ swiftWeather/swiftWeather/swiftWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftWeatherApp.swift
+//  swiftWeather
+//
+//  Created by Noah Frahm on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct swiftWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
